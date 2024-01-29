76b8cd0852a78dd2ee4ebe154005d489a2c528f7
Ella Mukhametshina <ella.mukhametshina@gmail.com>
2024-01-29 04:21:30 +0300

    выделен класс для работы с контактами ContactHelper

--- test/TestAddContact.py
+++ test/TestAddContact.py
@@ -11,10 +11,10 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.add_new_contact(Contact(firstname="Григорий", middlename="Евгеньевич", lastname="Врублевский",
-                             nickname="Hunter", title="person", company="MBQwerty",
-                             address="Нижний Новгород, улица Советская, дом 5", homephone="+7888999999",
-                             mobile_phone="+79819999999", work_phone="+7888777777", email1="hunter1@test.ru",
-                             email2="gr.evg.vr@test.ru", home_page="google.com", bday="12", bmonth="May", byear="1999",
-                             aday="12", amonth="May", ayear="2024"))
+    app.contact.add_new(Contact(firstname="Григорий", middlename="Евгеньевич", lastname="Врублевский",
+                                nickname="Hunter", title="person", company="MBQwerty",
+                                address="Нижний Новгород, улица Советская, дом 5", homephone="+7888999999",
+                                mobile_phone="+79819999999", work_phone="+7888777777", email1="hunter1@test.ru",
+                                email2="gr.evg.vr@test.ru", home_page="google.com", bday="12", bmonth="May", byear="1999",
+                                aday="12", amonth="May", ayear="2024"))
     app.session.logout()
