2f3b9d7872170dc07dc3c9a354556e4c5f6ac696
Ella Mukhametshina <ella.mukhametshina@gmail.com>
2024-02-13 03:06:30 +0300

    реализованы проверки в виде сравнения списков для групп

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,20 +4,34 @@ from model.group import Group
 def test_modify_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test", header="test", footer="test"))
-    app.group.modify_first_group(Group(name="modified_group", header="other_header", footer="other_footer"))
+    old_groups = app.group.get_group_list()
+    group = (Group(name="modified_group", header="other_header", footer="other_footer"))
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_modify_first_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test", header="test", footer="test"))
-    app.group.modify_first_group(Group(name="New_group"))
+    old_groups = app.group.get_group_list()
+    group = (Group(name="New_group"))
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
-def test_modify_first_group_header(app):
-    if app.group.count() == 0:
-        app.group.create(Group(name="test", header="test", footer="test"))
-    app.group.modify_first_group(Group(header="New_header"))
-
-def test_modify_first_group_footer(app):
-    if app.group.count() == 0:
-        app.group.create(Group(name="test", header="test", footer="test"))
-    app.group.modify_first_group(Group(header="New_footer"))
+# def test_modify_first_group_header(app):
+#     if app.group.count() == 0:
+#         app.group.create(Group(name="test", header="test", footer="test"))
+#     app.group.modify_first_group(Group(header="New_header"))
+#
+# def test_modify_first_group_footer(app):
+#     if app.group.count() == 0:
+#         app.group.create(Group(name="test", header="test", footer="test"))
+#     app.group.modify_first_group(Group(header="New_footer"))
