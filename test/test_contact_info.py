f913906497da86dd810ec26bd03abca97c041f94
Ella Mukhametshina <ella.mukhametshina@gmail.com>
2024-03-07 01:26:12 +0300

    доработан тест сравнения информации о контакте

--- test/test_contact_info.py
+++ test/test_contact_info.py
@@ -11,10 +11,10 @@ def test_contact_info_on_homepage(app):
     index = randrange(len(old_contacts))
     contact_from_homepage = app.contact.get_contact_list()[index]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_homepage.lastname == contact_from_edit_page.lastname
-    assert contact_from_homepage.firstname == contact_from_edit_page.firstname
+    assert clear_spaces(contact_from_homepage.lastname) == clear_spaces(contact_from_edit_page.lastname)
+    assert clear_spaces(contact_from_homepage.firstname) == clear_spaces(contact_from_edit_page.firstname)
     assert clear(contact_from_homepage.address) == clear(contact_from_edit_page.address)
-    assert contact_from_homepage.all_emails_from_homepage == merge_emails_like_on_homepage(contact_from_edit_page)
+    assert clear_spaces(contact_from_homepage.all_emails_from_homepage) == merge_emails_like_on_homepage(contact_from_edit_page)
     assert contact_from_homepage.all_phones_from_homepage == merge_phones_like_on_homepage(contact_from_edit_page)
 
 
@@ -31,6 +31,9 @@ def test_contact_info_on_homepage(app):
 
 def clear(s):
     return re.sub("[() -]","",s)
+
+def clear_spaces(s):
+    return re.sub("[ ]","",s)
 
 def merge_phones_like_on_homepage(contact):
     return "\n".join(filter(lambda x: x!="", map(lambda x: clear(x),
