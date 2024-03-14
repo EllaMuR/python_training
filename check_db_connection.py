96f02780978dc48cb79e364310fbcaec34638b20
Ella Mukhametshina <ella.mukhametshina@gmail.com>
2024-03-14 09:00:21 +0300

    добавлен тест на сравнение информации о контакте с домашней страницы и из бд

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,8 +1,10 @@
 import pymysql
 from fixture.db import DbFixture
+from fixture.orm import ORMFixture
+from model.group import Group
 
 #connection = pymysql.connect(host="127.0.0.1", database="addressbook", user="root", password="")
-db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 # try:
 #     groups=db.get_group_list()
@@ -12,17 +14,30 @@ db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 #
 # finally:
 #     db.destroy()
+#
+# try:
+#     contacts=db.get_contact_list()
+#     for contact in contacts:
+#         print(contact)
+#     print(len(contacts))
+#
+# finally:
+#     db.destroy()
 
+# try:
+#     l = db.get_contact_list()
+#     for item in l:
+#         print(item)
+#     print(len(l))
+# finally:
+#         pass
 try:
-    contacts=db.get_contact_list()
-    for contact in contacts:
-        print(contact)
-    print(len(contacts))
-
+    l = db.get_contacts_not_in_group(Group(id='65'))
+    for item in l:
+        print(item)
+    print(len(l))
 finally:
-    db.destroy()
-
-
+    pass
 
 
 # try:
--- test/test_contact_info.py
+++ test/test_contact_info.py
@@ -2,20 +2,41 @@ import re
 from model.contact import Contact
 from random import randrange
 
-def test_contact_info_on_homepage(app):
-    if app.contact.count() == 0:
+
+def test_match_contact_info_on_homepage_and_db(app, db):
+    if len(db.get_group_list()) == 0:
         app.contact.add_new(Contact(firstname="Тест", middlename="Тестович", lastname="Тестовый",
-                                nickname="Test",  homephone="+789456", email1="test1test@test.ru", bday="11",
-                                    bmonth="August", byear="2001"))
-    old_contacts = app.contact.get_contact_list()
-    index = randrange(len(old_contacts))
-    contact_from_homepage = app.contact.get_contact_list()[index]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_homepage.lastname == replace_many_spaces(contact_from_edit_page.lastname)
-    assert contact_from_homepage.firstname == replace_many_spaces(contact_from_edit_page.firstname)
-    assert contact_from_homepage.address == replace_many_spaces(contact_from_edit_page.address)
-    assert clear_spaces(contact_from_homepage.all_emails_from_homepage) == merge_emails_like_on_homepage(contact_from_edit_page)
-    assert contact_from_homepage.all_phones_from_homepage == merge_phones_like_on_homepage(contact_from_edit_page)
+                                    nickname="Test", title="test_person", company="test_test",
+                                    email1="test1test@test.ru", bday="11", bmonth="August", byear="2001"))
+    #db_contacts = db.get_contact_list()
+    contact_from_homepage = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
+    contacts_from_db = sorted(db.get_contact_list(), key=Contact.id_or_max)
+    l = len(db.get_contact_list())
+    for i in range(l):
+    #index = randrange(len(db_contacts))
+    # contact_from_homepage = app.contact.get_contact_list()[index]
+    # contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
+        assert contact_from_homepage[i].lastname == replace_many_spaces(contacts_from_db[i].lastname)
+        assert contact_from_homepage[i].firstname == replace_many_spaces(contacts_from_db[i].firstname)
+        assert contact_from_homepage[i].address == replace_many_spaces(contacts_from_db[i].address)
+        assert clear_spaces(contact_from_homepage[i].all_emails_from_homepage) == merge_emails_like_on_homepage(contacts_from_db[i])
+        assert contact_from_homepage[i].all_phones_from_homepage == merge_phones_like_on_homepage(contacts_from_db[i])
+
+
+# def test_contact_info_on_homepage(app):
+#     if app.contact.count() == 0:
+#         app.contact.add_new(Contact(firstname="Тест", middlename="Тестович", lastname="Тестовый",
+#                                 nickname="Test",  homephone="+789456", email1="test1test@test.ru", bday="11",
+#                                     bmonth="August", byear="2001"))
+#     old_contacts = app.contact.get_contact_list()
+#     index = randrange(len(old_contacts))
+#     contact_from_homepage = app.contact.get_contact_list()[index]
+#     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
+#     assert contact_from_homepage.lastname == replace_many_spaces(contact_from_edit_page.lastname)
+#     assert contact_from_homepage.firstname == replace_many_spaces(contact_from_edit_page.firstname)
+#     assert contact_from_homepage.address == replace_many_spaces(contact_from_edit_page.address)
+#     assert clear_spaces(contact_from_homepage.all_emails_from_homepage) == merge_emails_like_on_homepage(contact_from_edit_page)
+#     assert contact_from_homepage.all_phones_from_homepage == merge_phones_like_on_homepage(contact_from_edit_page)
 
 
 
